e929267acddc64820d83153cfccaaed3c1973509
seth spring <seth.c.spring@gmail.com>
2024-01-07 17:59:43 -0500

    Looping Code Along Complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const cardNames = ["Guadalupe", "Ollie", "Aki"];
+const messages = []
+
+function writeCards(cardNames) {
+   for (let i = 0; i < cardNames.length; i++) {
+      messages.push(`Thank you, ${cardNames[i]}, for the wonderful surprise gift!`)
+    }
+
+    return messages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+      console.log(number--)
+    }
+}
